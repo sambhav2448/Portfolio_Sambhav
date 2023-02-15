c1f839bfd7e24f6e9ed459914173c2c7ecb3d876
Sambhav Jain <55810174+sambhav2448@users.noreply.github.com>
2023-02-15 02:48:50 -0500

    updated resume link

--- src/components/section1/intro.js
+++ src/components/section1/intro.js
@@ -28,7 +28,7 @@ function Intro() {
               </p>
               <div className='intro_button'>
                 <a
-                  href='https://drive.google.com/file/d/1sBP9CaDKTi6fDtTVTn55F4pMRNhTsmRA/view?usp=sharing'
+                  href='https://drive.google.com/file/d/10rZP6ZQzSIC3t7DAEP8GUbJLW_-toSkM/view?usp=sharing'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
